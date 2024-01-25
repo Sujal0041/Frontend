3212fe045b189d033cd4ea994c2f0c69a0c0597d
Sujal0041 <152766716+Sujal0041@users.noreply.github.com>
2024-01-25 20:04:07 +0545

    New Page (Welcome Page) Created

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {View} from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import Welcomepage from './screens/Welcomepage';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
@@ -12,9 +13,10 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Welcome" component={Welcomepage} />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
---
+++ screens/Welcomepage.js
@@ -0,0 +1,24 @@
+// HomeScreen.js
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
