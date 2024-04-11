c7d8f8cd70f7a65fc63628e07e82db027eff7c75
Sujal0041 <152766716+Sujal0041@users.noreply.github.com>
2024-04-11 21:24:39 +0545

    New Screen instead of Drawer

---
+++ screens/More.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const More = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, React Native!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default More;
